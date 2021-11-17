c9ceb83d0c6528541cefe0799d2fbae4c7f4b7a2
Yman <64341841+Yman14@users.noreply.github.com>
2021-11-17 16:23:52 +0800

    Add files via upload

---
+++ create_db_n_table.php
@@ -0,0 +1,47 @@
+//Create a php program that will save the following student information into a database named: schooldb and table name studenttable
+
+<?php
+$servername = "localhost";
+$username = "uspfstudent";
+$password = "1234567";
+$dbname = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("<br>Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE schooldb";
+if ($conn->query($sql) === TRUE) {
+  echo "<br><br>Database schooldb created successfully";
+} else {
+  echo "<br>Error creating database: " . $conn->error;
+}
+
+mysqli_select_db ( $conn , "schooldb" );
+
+ //database name
+
+// Create connection
+
+// Check connection
+if (!$conn) {
+    die("<br>Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE studenttable (
+Student_Id_Number VARCHAR(15) NOT NULL, Student_Family_Name VARCHAR(50) NOT NULL, Student_First_name VARCHAR(50) NOT NULL, Student_Middle_Name VARCHAR(50) NOT NULL, Student_Email_Address VARCHAR(50) NOT NULL, Student_Home_Address VARCHAR(100) NOT NULL, Student_Mobile_number VARCHAR(15) NOT NULL, Student_Course VARCHAR(50) NOT NULL)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br>Table studenttable created successfully";
+} else {
+  echo "<br>Error creating table: " . $conn->error;
+}
+
+
+mysqli_close($conn);
+?>
